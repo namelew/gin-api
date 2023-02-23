146ef4f78c102ca455834baa0e990cf4f20bfae0
diogo <diogomaciel.cunha@gmail.com>
2023-02-23 09:01:49 -0300

    gin returning a json response

--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+func getAlunos(c *gin.Context) {
+	c.JSON(200, gin.H{"id": 1, "nome": "Diogo"})
+}
+
 func main() {
 	r := gin.Default()
+	r.GET("/alunos", getAlunos)
 	r.Run()
 }
